0384a67e779a4e28e676ba509d51f5d6e0a70fb0
Hayden Matthews <haydenamatthews@yahoo.com>
2019-04-18 16:48:00 -0600

    Added AI tank controllers and provided them a way to find the player tank.

---
+++ BattleTank/Source/BattleTank/TankAIController.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Tank.h"
+#include "AIModule/Classes/AIController.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+
+	void BeginPlay() override;
+private:
+	ATank* GetControlledTank() const;
+
+	ATank* GetPlayerTank() const;
+};
