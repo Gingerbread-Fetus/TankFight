f3ba23c346532e9751b9ab589db9ef70c5c675e7
Hayden Matthews <haydenamatthews@yahoo.com>
2019-04-30 15:27:13 -0600

    BT-20 Added blueprint callable method to set barrel. Added a method to test the location of that barrel.

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -3,8 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h"// Required put new includes above
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -20,6 +21,11 @@ public:
 
 	void AimAt(FVector HitLocation);
 
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
