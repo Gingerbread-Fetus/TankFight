0debabfd3ed041350b52850a083ff324d220b7d4
Hayden Matthews <haydenamatthews@yahoo.com>
2019-05-01 14:30:07 -0600

    BT-24-V46 Removed unneccessary tick methods. Added debug code to note time where things are breaking. Identified AimSolution bug.

--- BattleTank/Source/BattleTank/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/TankAimingComponent.cpp
@@ -57,6 +57,14 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
+
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found."), Time);
+	}
+	else
+	{
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: No solution found."), Time);
 	}
 	//Do nothing if no solution.
 }
