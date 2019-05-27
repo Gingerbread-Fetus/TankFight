cbb3195de0a54253d20f83936bbe4ae73cac8e0c
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2019-05-26 19:44:27 -0600

    BT40-V207 Added a navmesh to the Battleground.

--- BattleTank 4.22/Source/BattleTank/TankMovementComponent.cpp
+++ BattleTank 4.22/Source/BattleTank/TankMovementComponent.cpp
@@ -7,14 +7,14 @@
 
 void UTankMovementComponent::IntendMoveForward(float Throw) 
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw) 
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
 }
