0fa64c396181c8915b3cba5079a7e12bf832fb15
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2019-05-31 20:01:43 -0600

    BT52-V223 Dependency Mapping

--- BattleTank 4.22/Source/BattleTank/Tank.cpp
+++ BattleTank 4.22/Source/BattleTank/Tank.cpp
@@ -3,7 +3,6 @@
 #include "Tank.h"
 #include "TankBarrel.h"
 #include "Projectile.h"
-#include "TankMovementComponent.h"
 #include "TankAimingComponent.h"
 
 // Sets default values
--- BattleTank 4.22/Source/BattleTank/Tank.h
+++ BattleTank 4.22/Source/BattleTank/Tank.h
@@ -9,7 +9,6 @@
 
 class UTankBarrel;
 class UTankAimingComponent;
-class UTankMovementComponent;
 class UTankTurret;
 class AProjectile;
 
@@ -28,8 +27,6 @@ public:
 protected:
 	UPROPERTY(BlueprintReadOnly)
 	UTankAimingComponent* TankAimingComponent = nullptr;
-	UPROPERTY(BlueprintReadOnly)
-	UTankMovementComponent* TankMovementComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
